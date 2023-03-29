0f42d8832034d3134c8b3a9b15a24ca565deceb1
Kasra Safari <kasrasafari@gmail.com>
2023-03-28 23:52:42 -0400

    disable sidebar

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -41,6 +41,8 @@ const config = {
         blog: {
           routeBasePath: "/",
           showReadingTime: false,
+          blogSidebarCount: 0,
+          postsPerPage: "ALL"
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           // editUrl:
